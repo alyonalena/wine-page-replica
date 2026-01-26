1a899cb64393bace6381125e37e3f5fc2144a413
alyonalena <elenaflikema@yandex.ru>
2026-01-26 18:07:33 +0300

    tg sdk error

--- src/hooks/useTelegramId.ts
+++ src/hooks/useTelegramId.ts
@@ -4,17 +4,16 @@ import { retrieveLaunchParams } from '@telegram-apps/sdk'
 // Retrieve the launch parameters, including the initData raw string.
 
 
-
 export const useTelegramId = (): number => {
-  return useMemo(() => {
-    const launchParams = retrieveLaunchParams()
+  return useMemo(() => {    
     try {
-    // The user data is located within the initData
-    let telegramId = ''
-    if (launchParams?.initData?.user) {
-      telegramId = launchParams.initData.user?.id
-      console.log("User Telegram ID:", telegramId)
-    // Use the telegramId as needed in your application
+      const launchParams = retrieveLaunchParams()
+      // The user data is located within the initData
+      let telegramId = ''
+      if (launchParams?.initData?.user) {
+        telegramId = launchParams.initData.user?.id
+        console.log("User Telegram ID:", telegramId)
+      // Use the telegramId as needed in your application
     } else {
       console.error("User data not available in initData")
     }
