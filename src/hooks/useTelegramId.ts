939623ab516baa7121eb0c51a4528d942e6e6c0e
alyonalena <elenaflikema@yandex.ru>
2026-01-28 17:03:21 +0300

    Update useTelegramId.ts

--- src/hooks/useTelegramId.ts
+++ src/hooks/useTelegramId.ts
@@ -7,21 +7,18 @@ import { retrieveLaunchParams } from '@telegram-apps/sdk'
 export const useTelegramId = (): number => {
   return useMemo(() => { 
     let telegramId = ''
-    try {
-      const launchParams = retrieveLaunchParams()
-      // The user data is located within the initData
-      if (launchParams?.initData?.user) {
-        telegramId = launchParams.initData.user?.id
-        console.log("User Telegram ID:", telegramId)
-      // Use the telegramId as needed in your application
+    const launchParams = retrieveLaunchParams()
+    // The user data is located within the initData
+    if (launchParams?.initData?.user) {
+      telegramId = launchParams.initData.user?.id
+      console.log("User Telegram ID:", telegramId)
+    // Use the telegramId as needed in your application
     } else {
       console.error("User data not available in initData")
     }
     const localTelegramId = localStorage.getItem('telegramId')
+    
     return Number(telegramId || localTelegramId)
-    } catch(e) {
-      return 0
-    }
   }, [])
 }
 
