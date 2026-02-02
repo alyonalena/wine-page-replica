b8b84c98ebd8a24be917240cb4265254c7a592bb
alyonalena <elenaflikema@yandex.ru>
2026-02-03 01:18:33 +0300

    tg id

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,22 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { init, miniApp } from '@telegram-apps/sdk'
+
+const initializeTelegramSDK = async () => {
+    try {
+      await init()
+  
+      if (miniApp.ready.isAvailable()) {
+        await miniApp.ready()
+        console.log('Mini App готово')
+      }
+  
+    } catch (error) {
+      console.error('Ошибка инициализации:', error)
+    }
+  }
+  
+initializeTelegramSDK()
 
 createRoot(document.getElementById("root")!).render(<App />);
