adcd2bf67415ec6c60468c8fd331fde1ee538f5f
alyonalena <elenaflikema@yandex.ru>
2026-02-04 19:34:11 +0300

    tg id

--- src/hooks/useTelegramId.ts
+++ src/hooks/useTelegramId.ts
@@ -1,15 +1,17 @@
 import { useMemo } from 'react';
 //import { useLaunchParams } from '@telegram-apps/sdk-react'
 // Retrieve the launch parameters, including the initData raw string.
-
+import { useLaunchParams } from '@telegram-apps/sdk-react'
 
 export const useTelegramId = (): number => {
   return useMemo(() => { 
     let telegramId = 0
-    //const launchParams = useLaunchParams()
+    const launchParams = useLaunchParams()
     // The user data is located within the initData
-    if (window.Telegram?.WebApp) {
-      telegramId = window.Telegram.WebApp.initDataUnsafe.user.id
+    if (launchParams.tgWebAppData?.user?.ids
+      
+    ) {
+      telegramId = launchParams.tgWebAppData?.user?.id
       console.log("User Telegram ID:", telegramId)
     // Use the telegramId as needed in your application
     } else {
