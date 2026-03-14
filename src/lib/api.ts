6d8174e72f5b6475294ba96548cae8d54cbe077f
alyonalena <elenaflikema@yandex.ru>
2026-03-14 11:31:58 +0300

    personal info update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,6 +3,6 @@ const TG_API_BASE_URL = 'https://vfqc-bc18-fu02.gw-1a.dockhost.net/api';
 
 /** Returns the API base URL. Change the returned value below to switch backends. */
 export function getApiBaseUrl(): string {
-  return LOCAL_API_BASE_URL; // Switch to LOCAL_API_BASE_URL for local backend
+  return TG_API_BASE_URL; // Switch to LOCAL_API_BASE_URL for local backend
 }
 
